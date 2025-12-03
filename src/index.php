0904d7b912ad8c7cb68c5c9ebac8e0e8a72b062b
ugiramahirwegenereuse <ugiramahirwegenereuse@gmail.com>
2025-12-04 00:53:56 +0200

    d 4

---
+++ src/index.php
@@ -0,0 +1,23 @@
+<?php
+// index.php
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+
+?>
+<!doctype html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>ShareRide - Home</title>
+</head>
+<body>
+  <h1>Welcome to ShareRide (CODERWANDA)</h1>
+  <ul>
+    <li><a href="registration.php">Register</a></li>
+    <li><a href="login.php">Login</a></li>
+  </ul>
+</body>
+</html>
